0c9038ea0ae2f853c785f1c0c9f3e71aba8ff163
Egor And <georgand@yandex.ru>
2023-11-21 00:22:30 +0300

    S1R1 вынести serverError в константу

--- app.js
+++ app.js
@@ -1,14 +1,15 @@
-require('dotenv').config();
-const express = require('express');
-const mongoose = require('mongoose');
-const { errors } = require('celebrate');
-const bodyParser = require('body-parser');
-const cors = require('cors');
-const helmet = require('helmet');
-const { MONGO_URL, PORT } = require('./utils/config');
-const router = require('./router/index');
-const { requestLogger, errorLogger } = require('./middlewares/logger');
-const limiter = require('./middlewares/rateLimiter');
+require("dotenv").config();
+const express = require("express");
+const mongoose = require("mongoose");
+const { errors } = require("celebrate");
+const bodyParser = require("body-parser");
+const cors = require("cors");
+const helmet = require("helmet");
+const { MONGO_URL, PORT } = require("./utils/config");
+const router = require("./router/index");
+const { requestLogger, errorLogger } = require("./middlewares/logger");
+const limiter = require("./middlewares/rateLimiter");
+const serverError = require("./middlewares/serverError");
 
 const app = express();
 app.use(cors());
@@ -22,7 +23,7 @@ app.use(router);
 app.use(errorLogger);
 app.use(errors());
 
-app.use(require('./middlewares/serverError'));
+app.use(serverError);
 
 async function init() {
   await mongoose.connect(MONGO_URL);
