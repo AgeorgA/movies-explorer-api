f92d33170f27d8219c1bb2f0934076b77f551c6c
Egor And <georgand@yandex.ru>
2023-12-09 23:18:39 +0300

    куки

--- controllers/users.js
+++ controllers/users.js
@@ -63,7 +63,7 @@ module.exports.updateUser = (req, res, next) => {
 
 module.exports.login = (req, res, next) => {
   const { email, password } = req.body;
-  return User.findUserByCredentials(email, password)
+  return User.findUserByCredintails(email, password)
     .then((user) => {
       const token = jwt.sign({ _id: user._id }, JWT_SECRET, {
         expiresIn: '7d',
